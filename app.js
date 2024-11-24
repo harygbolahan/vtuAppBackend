ef3dddf6d66f89273ecc26a59a5eb8675244fc67
harygbolahan <47720548+harygbolahan@users.noreply.github.com>
2024-11-24 17:53:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 // app.use(cors("*"));
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace this with your frontend's URL
+  origin: 'https://vtu-app-frontend.vercel.app', // Replace this with your frontend's URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true // Allow cookies to be sent from the client if needed
 }));
